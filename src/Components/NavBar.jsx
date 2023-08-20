98a833b842437e9fb90617ed98ba8be57dddfce5
binayakbishnu <bishnu.binayak12@gmail.com>
2023-08-20 17:57:10 +0530

    feat: added logo and navbar logo navigation

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -12,6 +12,7 @@ import {
 import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
 import { BsFillPersonFill } from 'react-icons/bs'
 // import { BiTargetLock } from 'react-icons/bi'
+import logo from '../assets/logo.png'
 
 import { LoginLink, LogoutLink, SignupLink } from './Auth/CommonLinks'
 
@@ -38,9 +39,9 @@ export default function NavBar({ type, email }) {
         <header className="bg-none text-white">
             <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
                 <div className="flex lg:flex-1">
-                    <a href="/" className="-m-1.5 p-1.5">
+                    <a href={type == "loggedin" ? "/home" : "/"} className="-m-1.5 p-1.5">
                         <span className="sr-only">OneResume</span>
-                        <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+                        <img className="h-8 w-auto" src={logo} alt="" />
                     </a>
                 </div>
                 <div className="flex lg:hidden">
@@ -192,11 +193,11 @@ export default function NavBar({ type, email }) {
                 <Dialog.Panel
                     className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-[#191919] text-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
-                        <a href="#" className="-m-1.5 p-1.5">
+                        <a href={type == "loggedin" ? "/home" : "/"} className="-m-1.5 p-1.5">
                             <span className="sr-only">Your Company</span>
                             <img
                                 className="h-8 w-auto"
-                                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                                src={logo}
                                 alt=""
                             />
                         </a>
