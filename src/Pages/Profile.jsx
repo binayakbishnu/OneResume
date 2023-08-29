86d4f4c6be3563085e893465359d25d5aa2fb5b9
binayakbishnu <bishnu.binayak12@gmail.com>
2023-08-29 15:54:12 +0530

    : added null case for email in profile and wrapper

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -14,9 +14,9 @@ function Profile() {
         try {
             const q = query(collection(db, "users"), where("uid", "==", user?.uid));
             const doc = await getDocs(q);
-            const data = doc.docs[0].data();
+            const data = doc?.docs[0]?.data();
             // setName(data.name);
-            setEmail(data.email);
+            setEmail(data?.email);
         } catch (err) {
             console.error(err);
             alert("An error occured while fetching user data");
