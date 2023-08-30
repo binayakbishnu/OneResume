6643d327138e8464349b702fb76278f28271bdcd
binayakbishnu <bishnu.binayak12@gmail.com>
2023-08-31 01:49:18 +0530

    feat: removed fetchUserData and uid from profile

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 
-import { auth, db } from "../Authentication/firebase";
-import { query, collection, getDocs, where } from "firebase/firestore";
+import { auth, /* db */ } from "../Authentication/firebase";
+// import { query, collection, getDocs, where } from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
-import NavBar from '../Components/NavBar';
+// import NavBar from '../Components/NavBar';
 
 function Profile() {
-    const [user, loading, error] = useAuthState(auth);
-    const [email, setEmail] = useState("");
+    const [user, loading, /* error */] = useAuthState(auth);
 
+    /* const [email, setEmail] = useState("");
     const fetchUserData = async () => {
         try {
             const q = query(collection(db, "users"), where("uid", "==", user?.uid));
@@ -21,16 +21,13 @@ function Profile() {
             // console.error(err);
             // alert("An error occured while fetching user data");
         }
-    };
+    }; */
 
     const navigate = useNavigate()
     useEffect(() => {
         if (loading) return;
         if (!user) return navigate("/");
-        fetchUserData();
-
-        // localStorage.setItem(user?.uid, JSON.stringify(file));
-        // setUrl(localStorage.getItem('recent-image'));
+        // fetchUserData();
     }, [user, loading /* , file */]);
     return (
         <div className='text-white flex flex-col flex-1'>
@@ -45,14 +42,14 @@ function Profile() {
                 <p className='block md:hidden'>
                     {user?.email}
                 </p>
-                <p className='mt-5'>
+                {/* <p className='mt-5'>
                     Key: <span className='hidden md:inline cursor-pointer hover:bg-[#252525]' onClick={() => { navigator.clipboard.writeText(user?.uid) }}>
                         {user?.uid}
                     </span>
                 </p>
                 <p className='block md:hidden cursor-pointer hover:bg-[#252525] w-fit' onClick={() => { navigator.clipboard.writeText(user?.uid) }}>
                     {user?.uid}
-                </p>
+                </p> */}
             </div>
         </div>
     )
